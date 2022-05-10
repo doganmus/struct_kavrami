430c2966865cd2013c3a0a00b30f11cee69394ca
Dogan Mus <doganmus87@gmail.com>
2022-05-10 23:00:58 +0300

    ders sonu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace struct_kavrami
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+            Console.WriteLine("Class Alan Hesabı: {0}", dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_Struct;
+            dikdortgen_Struct.KisaKenar = 3;
+            dikdortgen_Struct.UzunKenar = 4;
+            Console.WriteLine("Struct Alan Hesabı: {0}", dikdortgen_Struct.AlanHesapla());
+
         }
+    }
+
+    class Dikdortgen{
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen(){
+            KisaKenar = 3;
+            UzunKenar = 4;
+        }
+
+        public long AlanHesapla(){
+            return this.KisaKenar*this.UzunKenar;
+        }
+    }
+    struct Dikdortgen_Struct{
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen_Struct(int kisaKenar, int uzunKenar)
+        {
+            KisaKenar = kisaKenar;
+            UzunKenar = uzunKenar;
+        }
+
+        public long AlanHesapla(){
+            return this.KisaKenar*this.UzunKenar;
+        }
+        
     }
 }
